c6d8d81c86bf771ff6770b74787cf7d187b34678
Luis Cortorreal <cort0038@algonquinlive.com>
2024-05-18 10:37:54 -0400

    refactor: Update logo in Navbar component for PeakTrail branding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 	return (
 		<nav className="flexBetween max-container padding-container relative z-30 py-5">
 			<Link href="/">
-				<Image src="/hilink-logo.svg" alt="logo" width={75} height={74} />
+				<Image src="/peaktrail-logo.svg" alt="logo" width={120} height={75} />
 			</Link>
 
 			<ul className="hidden h-full gap-12 lg:flex">
