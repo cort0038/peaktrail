a684f9fddbdedba43dfb7084116b58cb28d94b68
Luis Cortorreal <cort0038@algonquinlive.com>
2024-05-18 23:47:57 -0400

    refactor: Optimize Camp component with lazy loading for images

--- app/components/Camp.tsx
+++ app/components/Camp.tsx
@@ -1,77 +1,102 @@
-import {PEOPLE_URL} from "@/app/constants"
+"use client"
+
+import {useRef} from "react"
 import Image from "next/image"
+import {FaArrowCircleRight, FaArrowCircleLeft} from "react-icons/fa"
+import {CAMPS} from "../constants"
 
 type CampProps = {
 	backgroundImage: string
 	title: string
 	subtitle: string
 	peopleJoined: string
+	peopleUrls: string[]
 }
 
-const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) => {
+const CampSite = ({backgroundImage, title, subtitle, peopleJoined, peopleUrls}: CampProps) => {
+	const shuffledPeopleUrls = shuffleArray(peopleUrls)
+
 	return (
 		<div
-			className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+			className={`flex h-full w-full min-w-[385px] md:min-w-[600px] ${backgroundImage} bg-no-repeat bg-cover rounded-5xl 2xl:rounded-5xl border border-white`}>
 			<div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
 				<div className="flexCenter gap-4">
-					<div className="rounded-full bg-green-50 p-4">
-						<Image src="/folded-map.svg" alt="map" width={28} height={28} />
+					<div className="rounded-full">
+						<Image src="/location.svg" alt="map" width={28} height={28} />
 					</div>
-					<div className="flex flex-col gap-1">
-						<h4 className="bold-18 text-white">
-							{title}
-							<p className="regular-14 text-white">{subtitle}</p>
-						</h4>
+					<div className="flex flex-col text-white text-[1.2rem] bg-black/60 rounded p-1 w-fit">
+						<p className="font-bold ">{title}</p>
+						<p className="font-normal text-[1rem]">{subtitle}</p>
 					</div>
 				</div>
-				<div className="flexCenter gap-6">
+				<div className="flexCenter gap-2">
 					<span className="flex -space-x-4 overflow-hidden">
-						{PEOPLE_URL.map(url => (
+						{shuffledPeopleUrls.map(url => (
 							<Image
 								src={url}
 								key={url}
 								alt="Person"
-								width={52}
-								height={52}
-								className="inline-block h-10 w-10 rounded-full"
+								width={14}
+								height={14}
+								className="inline-block h-14 w-14 rounded-full border border-white object-cover"
 							/>
 						))}
 					</span>
-					<p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+					<p className="font-semibold md:font-extrabold text-[1rem] md:text-[1.2rem] bg-black/60 rounded px-1 w-fit text-white">
+						{peopleJoined}
+					</p>
 				</div>
 			</div>
 		</div>
 	)
 }
 
+const shuffleArray = (array: any[]) => {
+	const shuffledArray = [...array]
+	for (let i = shuffledArray.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		;[shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
+	}
+	return shuffledArray
+}
+
 const Camp = () => {
+	const scrollContainerRef = useRef<HTMLDivElement>(null)
+
+	const scroll = (direction: "left" | "right") => {
+		if (scrollContainerRef.current) {
+			const {scrollLeft, clientWidth} = scrollContainerRef.current
+			const scrollAmount = clientWidth - 20
+			scrollContainerRef.current.scrollTo({
+				left: direction === "left" ? scrollLeft - scrollAmount : scrollLeft + scrollAmount,
+				behavior: "smooth"
+			})
+		}
+	}
+
 	return (
-		<section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-			<div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
-				<CampSite
-					backgroundImage="bg-bg-img-1"
-					title="Putuk Truno Camp"
-					subtitle="Prigen, Pasuruan"
-					peopleJoined="50+ Joined"
-				/>
-				<CampSite
-					backgroundImage="bg-bg-img-2"
-					title="Mountain View Camp"
-					subtitle="Somewhere in the Wilderness"
-					peopleJoined="50+ Joined"
-				/>
+		<section className="max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+			<div
+				className="hide-scrollbar flex h-[340px] w-full items-start justify-start overflow-x-auto lg:h-[400px] xl:h-[640px] md:-space-x-20"
+				ref={scrollContainerRef}>
+				{CAMPS.map((camp, index) => (
+					<CampSite
+						key={index}
+						backgroundImage={camp.backgroundImage}
+						title={camp.title}
+						subtitle={camp.subtitle}
+						peopleJoined={camp.peopleJoined}
+						peopleUrls={camp.peopleUrls}
+					/>
+				))}
 			</div>
-			<div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
-				<div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
-					<h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
-						<strong>Feeling Lost</strong> And Not Knowing The Way?
-					</h2>
-					<p className="regular-14 xl:regular-16 mt-5 text-white">
-						Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of
-						getting lost is very large. That's why we are here for those of you who want to start an adventure
-					</p>
-					<Image src="/quote.svg" alt="Camp-2" width={186} height={219} className="camp-quote" />
-				</div>
+			<div className="flex justify-between mt-4">
+				<button onClick={() => scroll("left")} className=" text-md mx-8">
+					<FaArrowCircleLeft />
+				</button>
+				<button onClick={() => scroll("right")} className="  text-md mx-8">
+					<FaArrowCircleRight />
+				</button>
 			</div>
 		</section>
 	)
