f68c75c6e427f21a35e1ad8b4860ae5fd8f3e730
Luis Cortorreal <cort0038@algonquinlive.com>
2024-05-19 23:17:06 -0400

    refactor: Add ToTop and ProgressBar components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Features from "@/app/components/Features"
 import GetApp from "@/app/components/GetApp"
 import Guide from "@/app/components/Guide"
 import Hero from "@/app/components/Hero"
+import ToTop from "@/app/utils/ToTop"
+import ProgressBar from "./utils/ProgressBar"
 
 export default function Home() {
 	return (
@@ -12,6 +14,8 @@ export default function Home() {
 			<Guide />
 			<Features />
 			<GetApp />
+			<ToTop />
+			<ProgressBar />
 		</>
 	)
 }
