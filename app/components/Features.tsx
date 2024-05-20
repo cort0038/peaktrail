0e0abaf222e26580265553c3459ce2f6a5132bf2
Luis Cortorreal <cort0038@algonquinlive.com>
2024-05-19 23:18:25 -0400

    style: Update Features component with new phone image and styles

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -3,21 +3,22 @@ import Image from "next/image"
 
 const Features = () => {
 	return (
-		<section className=" flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-			<div className="flex max-container padding-container relative w-full justify-end">
+		<section className=" flex-col flexCenter" id="services">
+			<div className="flex max-container padding-container relative w-full justify-end z-20">
+				<div className="feature-map" />
 				<div className="flex flex-1 lg:min-h-[900px]">
-					<Image src="/phone.png" alt="phone" width={440} height={1000} className="feature-phone" />
+					<Image src="/phone-1.png" alt="phone" width={440} height={1000} className="feature-phone" />
 				</div>
-				<div className="z-20 flex w-full flex-col lg:w-[60%]">
+				<div className="z-20 flex w-full flex-col xl:w-[60%]">
 					<div className="relative">
 						<Image
 							width={50}
 							height={50}
 							alt="camp"
 							src="/camp.svg"
-							className="absolute left-[-5px] top-[-28px] w-10 lg:w-50"
+							className="absolute left-[3px] top-[-40px] lg:top-[-50px] w-16 lg:w-[50px]"
 						/>
-						<h2 className="bold-40 lg:bold-64">Our Features</h2>
+						<h2 className="section-title">Our Features</h2>
 					</div>
 					<ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20">
 						{FEATURES.map(feature => (
@@ -44,11 +45,11 @@ type FeatureItem = {
 const FeatureItem = ({title, icon, description}: FeatureItem) => {
 	return (
 		<li className="flex w-full flex-1 flex-col items-start">
-			<div className="rounded-full p-4 lg:pd-7 bg-green-50">
+			<div className="rounded-full p-3 lg:pd-7 bg-blue-200">
 				<Image src={icon} alt="map" width={28} height={28} />
 			</div>
-			<h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
-			<p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">{description}</p>
+			<h2 className="font-extrabold text-xs md:text-sm mt-2 capitalize">{title}</h2>
+			<p className="mt-2 lg:bg-none">{description}</p>
 		</li>
 	)
 }
