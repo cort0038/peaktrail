511a77e003fdd0ee41fc03975db7b006476af379
Luis Cortorreal <cort0038@algonquinlive.com>
2024-05-19 23:16:55 -0400

    refactor: Add suppressHydrationWarning to html lang attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body className={inter.className}>
 				<Navbar />
 				<main className="relative overflow-hidden">{children}</main>
