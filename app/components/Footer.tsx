7b660307dc67184c715009f4fde74d0e7613622d
Luis Cortorreal <cort0038@algonquinlive.com>
2024-05-19 23:18:35 -0400

    style: Update logo in Footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,16 +4,16 @@ import Link from "next/link"
 
 const Footer = () => {
 	return (
-		<footer className="flexCenter mb-24">
+		<footer className="flexCenter pb-24" id="footer">
 			<div className="padding-container flex w-full max-container flex-col gap-14">
 				<div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
 					<Link href="/" className="mb-10">
-						<Image src="/peaktrail-logo.svg" alt="logo" width={120} height={29} />
+						<Image src="/logo.svg" alt="logo" width={140} height={29} />
 					</Link>
 					<div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
 						{FOOTER_LINKS.map(columns => (
 							<FooterColumn title={columns.title} key={columns.title}>
-								<ul className="regular-14 flex flex-col gap-4 text-gray-30">
+								<ul className=" flex flex-col gap-4 text-gray-30">
 									{columns.links.map(link => (
 										<Link href="/" key={link}>
 											{link}
@@ -35,7 +35,7 @@ const Footer = () => {
 
 						<div className="flex flex-col gap-5">
 							<FooterColumn title={SOCIALS.title}>
-								<ul className="regular-14 flex gap-4 text-gray-30">
+								<ul className="flex gap-4 text-gray-30">
 									{SOCIALS.links.map(link => (
 										<Link href="/" key={link}>
 											<Image src={link} alt="logo" width={24} height={24} />
@@ -47,7 +47,7 @@ const Footer = () => {
 					</div>
 				</div>
 				<div className="border bg-gray-20" />
-				<p className="regular-14 w-full text-center text-gray-30">
+				<p className="w-full text-center text-gray-30">
 					&copy;
 					{new Date().getFullYear()} PeakTrail. All rights reserved.
 				</p>
@@ -64,7 +64,7 @@ type FooterColumnProps = {
 const FooterColumn = ({title, children}: FooterColumnProps) => {
 	return (
 		<div className="flex flex-col gap-5">
-			<h4 className="bold-18 whitespace-nowrap">{title}</h4>
+			<h4 className="font-bold whitespace-nowrap">{title}</h4>
 			{children}
 		</div>
 	)
