dd8fa2745aa16e4a96217c06eff572b8a98f2912
Luis Cortorreal <cort0038@algonquinlive.com>
2024-05-19 23:19:02 -0400

    refactor: Move Hero component to utils folder and update styles

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image"
-import Button from "./Button"
+import Button from "../utils/Button"
 
 const Hero = () => {
 	return (
-		<section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 lg:flex-row">
+		<section className="max-container padding-container flex flex-col gap-20 py-40 md:gap-28 lg:py-60 lg:flex-row">
 			<div className="hero-map" />
 
 			{/* {LEFT} */}
@@ -13,41 +13,41 @@ const Hero = () => {
 					alt="camp"
 					width={50}
 					height={50}
-					className="absolute left-[3px] top-[-50px] w-10 lg:w-[50px]"
+					className="absolute left-[3px] top-[-40px] lg:top-[-50px] w-16 lg:w-[50px]"
 				/>
 				<div className="flex flex-col gap-6">
-					<h1 className="font-[900] leading-[1.1] md:text-lg text-sm">Woodland Park</h1>
+					<h1 className="section-title">Woodland Park</h1>
 					<p className="mt-2 text-teal-900 lg:max-w-[520px]">
 						We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of
-						nature. We can help you on an adventure around the world in just one app
+						nature. We can help you on an adventure around the world in just one app.
 					</p>
 				</div>
 
-				<div className="flex gap-10 flex-col">
+				<div className="flex gap-10 flex-col ">
 					<div className="my-6 flex flex-wrap gap-5">
-						<div className="flex items-center">
-							{Array(5)
-								.fill(1)
-								.map((_, index) => (
-									<Image src="/star.svg" key={index} width={24} alt="star" height={24} />
-								))}
+						<div className="rating items-center">
+							<input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+							<input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+							<input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+							<input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+							<input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
 						</div>
-						<p className="bold-16 lg:bold-20 text-blue-700">
+						<p className="text-blue-800">
 							198K
 							<span className="ml-1">Excellent Reviews</span>
 						</p>
 					</div>
 
 					<div className="flex flex-col w-full gap-3 sm:flex-row">
-						<Button type="button" title="Download App" variant="btn_green" />
-						<Button type="button" icon="/play.svg" title="How We Work?" variant="btn_white_text" />
+						<Button type="button" title="Download App" variant="btn_blue" />
+						<Button type="button" icon="/play.svg" title="How We Work?" variant="btn_blue_outline" />
 					</div>
 				</div>
 			</div>
 
 			{/* {RIGHT} */}
 			<div className="relative flex flex-1 items-start">
-				<div className="flex relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-blue-100 px-7 py-8">
+				<div className="flex relative left-24 lg:left-3 z-20 w-[268px] flex-col gap-8 rounded-3xl bg-blue-100 px-7 py-8">
 					<div className="flex flex-col ">
 						<div className="flexBetween">
 							<p className="font-bold text-black">Location</p>
@@ -68,6 +68,7 @@ const Hero = () => {
 						</div>
 					</div>
 				</div>
+				<Image src="/map.png" height={350} width={350} alt="map" className="hidden lg:flex" />
 			</div>
 		</section>
 	)
