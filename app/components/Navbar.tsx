d27977ab63640db5653d5eb8b381d25af56d9f14
Luis Cortorreal <cort0038@algonquinlive.com>
2024-05-19 23:19:11 -0400

    refactor: Move Navbar Button component to utils folder and update logo and styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image"
 import Link from "next/link"
 import {NAV_LINKS} from "@/app/constants"
-import Button from "./Button"
+import Button from "../utils/Button"
 
 const Navbar = () => {
 	return (
 		<nav className="flexBetween max-container padding-container relative z-30 py-5">
 			<Link href="/">
-				<Image src="/peaktrail-logo.svg" alt="logo" width={120} height={75} />
+				<Image src="/logo.svg" alt="logo" width={140} height={75} />
 			</Link>
 
 			<ul className="hidden h-full gap-12 lg:flex">
@@ -15,14 +15,14 @@ const Navbar = () => {
 					<Link
 						href={link.href}
 						key={link.key}
-						className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+						className="flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
 						{link.label}
 					</Link>
 				))}
 			</ul>
 
 			<div className="lg:flexCenter hidden">
-				<Button type="button" title="Login" icon="/user.svg" variant="btn_dark_green" />
+				<Button type="button" title="Join Us" icon="/user.svg" variant="btn_blue" linkhref="#download" />
 			</div>
 
 			<Image src="menu.svg" alt="menu" width={32} height={32} className="inline-block cursor-pointer lg:hidden" />
